32daba9e9560db7e05e82f605d5d2121da21e984
Josh Sokol <josh@simplerisk.com>
2019-06-15 00:16:00 -0500

    Update

--- ubuntu-1804-setup.sh
+++ ubuntu-1804-setup.sh
@@ -115,17 +115,17 @@ print_status "Generating MySQL passwords..."
 exec_cmd "apt-get install -y pwgen > /dev/null 2>&1"
 NEW_MYSQL_ROOT_PASSWORD=`pwgen -c -n -1 20` > /dev/null 2>&1
 MYSQL_SIMPLERISK_PASSWORD=`pwgen -c -n -1 20` > /dev/null 2>&1
-echo "MYSQL ROOT PASSWORD: ${NEW_MYSQL_ROOT_PASSWORD}" >> /tmp/passwords.txt
-echo "MYSQL SIMPLERISK PASSWORD: ${MYSQL_SIMPLERISK_PASSWORD}" >> /tmp/passwords.txt
+echo "MYSQL ROOT PASSWORD: ${NEW_MYSQL_ROOT_PASSWORD}" >> /root/passwords.txt
+echo "MYSQL SIMPLERISK PASSWORD: ${MYSQL_SIMPLERISK_PASSWORD}" >> /root/passwords.txt
 
 print_status "Configuring MySQL..."
 exec_cmd "sed -i '$ a sql-mode=\"STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION\"' /etc/mysql/mysql.conf.d/mysqld.cnf > /dev/null 2>&1"
 exec_cmd "mysql -uroot mysql -e \"CREATE DATABASE simplerisk\""
 exec_cmd "mysql -uroot simplerisk -e \"\\. /var/www/simplerisk/install/db/simplerisk-en-${CURRENT_SIMPLERISK_VERSION}.sql\""
 exec_cmd "mysql -uroot simplerisk -e \"GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER ON simplerisk.* TO 'simplerisk'@'localhost' IDENTIFIED BY '${MYSQL_SIMPLERISK_PASSWORD}'\""
-#exec_cmd "mysql -uroot mysql -e \"UPDATE user SET authentication_string=PASSWORD('${NEW_MYSQL_ROOT_PASSWORD}') WHERE user='root'\""
 exec_cmd "mysql -uroot mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${NEW_MYSQL_ROOT_PASSWORD}'\""
-exec_cmd "mysql -uroot mysql -e \"FLUSH PRIVILEGES\""
+#exec_cmd "mysql -uroot mysql -e \"UPDATE user SET authentication_string=PASSWORD('${NEW_MYSQL_ROOT_PASSWORD}') WHERE user='root'\""
+#exec_cmd "mysql -uroot mysql -e \"FLUSH PRIVILEGES\""
 
 print_status "Setting the SimpleRisk database password..."
 exec_cmd "sed -i \"s/DB_PASSWORD', 'simplerisk/DB_PASSWORD', '${MYSQL_SIMPLERISK_PASSWORD}/\" /var/www/simplerisk/includes/config.php > /dev/null 2>&1"
@@ -139,7 +139,7 @@ exec_cmd "ufw allow http > /dev/null 2>&1"
 exec_cmd "ufw allow https > /dev/null 2>&1"
 exec_cmd "ufw --force enable > /dev/null 2>&1"
 
-print_status "Check /tmp/passwords.txt for the MySQL root and simplerisk passwords."
+print_status "Check /root/passwords.txt for the MySQL root and simplerisk passwords."
 print_status "INSTALLATION COMPLETED SUCCESSFULLY"
 
 #echo "Updating the latest packages..."
